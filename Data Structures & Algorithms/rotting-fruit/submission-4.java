542681fec3379cedd575c615c4359e6904a200e9
Lorenzo Rodriguez <lr72@rice.edu>
2026-08-05 15:44:04 -0500

    Add: rotting-fruit - submission-4

---
+++ Data Structures & Algorithms/rotting-fruit/submission-4.java
@@ -0,0 +1,59 @@
+class Solution {
+    public int orangesRotting(int[][] grid) {
+        Queue<int[]> queue = new ArrayDeque<>();
+        int mins = -1;
+        boolean hasFruit = false;
+
+        for (int row = 0; row < grid.length; row++) {
+            for (int col = 0; col < grid[0].length; col++) {
+                if (grid[row][col] == 2) {
+                    queue.offer(new int[]{row, col});
+                } else if (grid[row][col] == 1) {
+                    hasFruit = true;
+                }
+            }
+        }
+
+        if (!hasFruit) {
+            return 0;
+        }
+
+        int[][] directions = new int[][]{{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
+        
+
+        while (!queue.isEmpty()) {
+            int levelSize = queue.size();
+
+            for (int i = 0; i < levelSize; i++) {
+                int[] curr = queue.poll();
+
+                int row = curr[0];
+                int col = curr[1];
+                
+                // Explore surroundings
+                for (int[] direction: directions) {
+                    int newRow = row + direction[0];
+                    int newCol = col + direction[1];
+
+                    if (newRow > -1 && newRow < grid.length && newCol > -1 && newCol < grid[0].length && grid[newRow][newCol] == 1) {
+                        grid[newRow][newCol] = 2;
+                        queue.offer(new int[]{newRow, newCol});
+                    }
+                }
+            }
+
+            mins++;
+        }
+
+        // Check for no fresh fruits
+        for (int row = 0; row < grid.length; row++) {
+            for (int col = 0; col < grid[0].length; col++) {
+                if (grid[row][col] == 1) {
+                    return -1;
+                }
+            }
+        }
+
+        return mins;
+    }
+}
